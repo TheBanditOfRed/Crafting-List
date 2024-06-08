5331c2e85bc41c06a600328c48c3db5a7e7f0ebd
TheBanditOfRed <57039992+TheBanditOfRed@users.noreply.github.com>
2024-06-08 01:00:31 +0100

    Updated UI and cleaned up code

--- main.py
+++ main.py
@@ -1,51 +1,100 @@
 import csv
 import yaml
+import math
 
 
-
-
-print("")
-print("")
-print("")
-print(" ___________________________________________________________________________________________")
-print('|   _____            __ _   _                _____      _            _       _              |')
-print('|  / ____|          / _| | (_)              / ____|    | |          | |     | |             |')
-print('| | |     _ __ __ _| |_| |_ _ _ __   __ _  | |     __ _| | ___ _   _| | __ _| |_ ___  _ __  |')
-print("| | |    | '__/ _` |  _| __| | '_ \ / _` | | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__| |")
-print("| | |____| | | (_| | | | |_| | | | | (_| | | |___| (_| | | (__| |_| | | (_| | || (_) | |    |")
-print("|  \_____|_|  \__,_|_|  \__|_|_| |_|\__, |  \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|    |")
-print("|                                    __/ |                                                  |")
-print("|                                   |___/                                                   |")
-print("|___________________________________________________________________________________________|")
-print("")
-print("")
-print("")
-
-with open("resources.yaml", "r") as yaml_file:
-    resources = yaml.safe_load(yaml_file)
-
-with open('testing/test.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+with open("resources.yaml", "r") as resources_yaml:
+    resources = yaml.safe_load(resources_yaml)
     
-    next(csv_reader)
-    
-    for row in csv_reader:
-        item = row[0]
-        amount = row[1]
-        #dict_item = resources["resources"][item]
-        print("")
-        print(item)
-        print("Select Process:")
-        recipes = resources['resources'][item]['recipes']
-        #print(recipe_type)
-        for x in range(len(recipes)):
-            recipe_type = recipes[x].get("recipe_type")
-            print(x + 1, ". ", recipe_type)
+with open("config.yaml", "r") as config_yaml:
+    config = yaml.safe_load(config_yaml)
+
+
+def startup_icon():
+    print("")
+    print("")
+    print("")
+    print(" ___________________________________________________________________________________________")
+    print("|                                                                                           |")
+    print('|   _____            __ _   _                _____      _            _       _              |')
+    print('|  / ____|          / _| | (_)              / ____|    | |          | |     | |             |')
+    print('| | |     _ __ __ _| |_| |_ _ _ __   __ _  | |     __ _| | ___ _   _| | __ _| |_ ___  _ __  |')
+    print(r"| | |    | '__/ _` |  _| __| | '_ \ / _` | | |    / _` | |/ __| | | | |/ _` | __/ _ \| '__| |")
+    print("| | |____| | | (_| | | | |_| | | | | (_| | | |___| (_| | | (__| |_| | | (_| | || (_) | |    |")
+    print(r"|  \_____|_|  \__,_|_|  \__|_|_| |_|\__, |  \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|    |")
+    print("|                                    __/ |                                                  |")
+    print("|                                   |___/                         Created by TheBanditOfRed |")
+    print("|                                                                Assisted by conallkavanagh |")
+    print("|___________________________________________________________________________________________|")
+    print("")
+    print("")
+    print("")
+ 
+#will be needed at some point in future
+'''if config["Display Amount"].get("Stack") == 1:
+    stack_size = 64
+elif config["Display Amount"].get("Shulker Box") == 1:
+    stack_size = 1728
+elif config["Display Amount"].get("Double Chest") == 1:
+    stack_size = 3456'''
+
+
+def process_selection(resources, config):
+    #try:
+        with open('testing/test.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
             
-        recipe_selection = input('> ')
+            next(csv_reader)
+            
+            for row in csv_reader:
+                item = row[0]
+                amount = int(row[1])
+                
+                stack_size = 64
+                
+                num_stacks = math.trunc(amount / stack_size)
+                num_items = amount - (num_stacks * stack_size)
+                
+                print("")
+                if num_stacks == 1:
+                    print(item, ": ", num_stacks, "Stack +", num_items)
+                else:
+                    print(item, ": ", num_stacks, "Stacks +", num_items)
+                    
+                    
+                print("Select Process:")
+                recipes = resources['resources'][item]['recipes']
+                
+                
+                for x in range(len(recipes)):
+                    recipe_type = recipes[x].get("recipe_type")
+                    print("[" + str(x + 1) + "]", recipe_type)
+                    
+                recipe_selection = input('> ')
+                
+                while recipe_selection.isnumeric() == False or not 1 <= int(recipe_selection) <= len(recipes):
+                    print(f"Invalid Entry: Please enter a number from 1 to {len(recipes)}")
+                    recipe_selection = input('> ')
+            
+                recipe_selection = int(recipe_selection)
+                
+    #except:
+    #    print("No material list found")
         
-        while recipe_selection.isnumeric() == False or not 1 <= int(recipe_selection) <= len(recipes):
-            print(f"Invalid Entry: Please enter a number from 1 to {len(recipes)}")
-            recipe_selection = input('> ')
+def menu(resources, config):
+    print("|    [1] Start Calculation    |    [2] Edit Config    |")
+    menu_sel = input("> ")
     
-        recipe_selection = int(recipe_selection)
+    while menu_sel.isnumeric() == False or not 1 <= int(menu_sel) <= 2:
+        print("Invalid Command")
+        menu_sel = input('> ')
+        
+    menu_sel = int(menu_sel)
+                    
+    if menu_sel == 1:
+        process_selection(resources, config)
+    elif menu_sel == 2:
+        print("DO CONFIG FILE CONFIG WHEN YOU LEARN HOW TO EDIT YAML FILES")
+
+startup_icon()
+menu(resources, config)
