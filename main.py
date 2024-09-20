f6fd234511b91f614577c5acebbd722dcc031f3c
Oscar BN <57039992+TheBanditOfRed@users.noreply.github.com>
2024-09-20 02:57:44 +0100

    Process Saving, Error Handling, & more
    Process selection saves to its own file.
    Added error handling to a lot of new inputs.
    File selection screen.
    Process list duplicate name handling when making new file not working.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import csv
-import easygui
+import tkinter as tk
+from tkinter import filedialog
 import json
 import math
+import os
 import datetime
+
+root = tk.Tk()
 
 # Load main files
 #with open("config.json", "r") as config_json:
@@ -28,7 +32,7 @@ def startup_icon():
     print("|                                    __/ |                                                  |")
     print("|                                   |___/                         Created by TheBanditOfRed |")
     print("|                                                                Assisted by conallkavanagh |")
-    print("|___________________________________________________________________________________________|")
+    print("|___________________________________________________________________________________________|")#15
     print("")
     print("")
     print("")
@@ -48,11 +52,16 @@ def data_format():
         'multiplier': '',
     }
 
+def add_new_item():
+    print('To be done')
+    root.mainloop(
+        startup_icon(),
+        menu(resources, config)
+        )
 
-def process_selection(resources, config, csv_path):
-    #try:
-        # Load exported csv material list
-        with open(csv_path, 'r') as csv_file:
+def process_selection(resources, config, path, save_file_path):
+    try:
+        with open(path, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             next(csv_reader)
             
@@ -74,7 +83,34 @@ def process_selection(resources, config, csv_path):
                     
                     
                 print("Select Process:")
-                recipes = resources['resources'][item]['recipes']
+                try:
+                    recipes = resources['resources'][item]['recipes']
+                except:
+                    print("The following item", item, "does not exist in resources.json")
+                    print("Would you like to add it? (Y/N)")
+                    add_new_item_input = input("> ")
+                    add_new_item_input = add_new_item_input.lower().strip('eso')
+                    while add_new_item_input not in ['y', 'n']:
+                        print("Invalid Entry: Please select [Y]es or [N]o")
+                        add_new_item_input = input('> ')
+                        add_new_item_input = add_new_item_input.lower().strip('es')
+                    if add_new_item_input == 'y':
+                        add_new_item(item)
+                    elif add_new_item_input == 'n':
+                        print('Would you like to [S]kip this item, or return to the [M]enu')
+                        add_new_item_input = input('> ')
+                        add_new_item_input = add_new_item_input.lower().strip('kipenu')
+                        while add_new_item_input not in ['s', 'm']:
+                            print("Invalid Entry: Please select [S]kip or [M]enu")
+                            add_new_item_input = input('> ')
+                            add_new_item_input = add_new_item_input.lower().strip('kipenu')
+                        if add_new_item_input == 's':
+                            continue
+                        elif add_new_item_input == 'm':
+                            root.mainloop(
+                                startup_icon(),
+                                menu(resources, config)
+                                )
                 
                 for x in range(len(recipes)):
                     recipe_type = recipes[x].get("recipe_type")
@@ -110,46 +146,171 @@ def process_selection(resources, config, csv_path):
                     recipe_selection = input('> ')
             
                 recipe_selection = int(recipe_selection) - 1
-            
+
+                with open(save_file_path) as file_path:
+                    add_processes = json.load(file_path)
+
+                add_processes.append({
+                    "Item": item,
+                    "Process": recipe_selection,
+                })
+
+                print(add_processes)
+
+                with open(save_file_path, 'w') as json_file:
+                    json.dump(add_processes, json_file,
+                            indent=4,
+                            separators=(',',': '))
+                    
+            #file "saving" (more properly file deleting but makes more sense to the user this way)
             print("Save process selection? (Y/N)")
             file_save_selection = input('> ')
-
-            accepted_responses = [
-                'yes', 'Yes', 'y', 'Y'
-            ]
-
-            #if file_save_selection in accepted_responses:
+            file_save_selection = file_save_selection.lower().strip('eso')
+            while file_save_selection not in ['y', 'n']:
+                print("Invalid Entry: Please select [Y]es or [N]o")
+                file_save_selection = input('> ')
+                file_save_selection = file_save_selection.lower().strip('es')
+            if file_save_selection == 'y':
+                print(r"File saved to ~\saved-process")
+            elif file_save_selection == 'n':
+                print('Are you sure you want to delete the process list? (Y/N)')
+                file_save_selection = input('> ')
+                file_save_selection = file_save_selection.lower().strip('eso')
+                while file_save_selection not in ['y', 'n']:
+                    print("Invalid Entry: Please select [Y]es or [N]o")
+                    file_save_selection = input('> ')
+                    file_save_selection = file_save_selection.lower().strip('es')
+                if file_save_selection == 'y':
+                    os.remove('saved-process/new_process_selection.json')
+                elif file_save_selection == 'n':
+                    print(r"File saved to ~\saved-process")
+            
+            root.mainloop(
+                startup_icon(),
+                menu(resources, config)
+                )
                 
-    #except:
-    #    print("No material list found")
-        
+    except:
+        print("No material list selected")
+        print("Returning to menu")
+        root.mainloop(
+            startup_icon(),
+            menu(resources, config)
+            )
+
+def load(save_file_path):
+    print('To be done')
+    root.mainloop(
+        startup_icon(),
+        menu(resources, config)
+        )
+
 def menu(resources, config):
-    print("|    [1] Start Calculation    |    [2] Edit Config    |    [3] Load Crafting List    |")
+    print("|   [1] Start Calculation   |   [2] Settings   |   [3] Load Crafting List   |   [4] Exit   |")
     menu_sel = input("> ")
     
     # Invalid entry handeling
-    while menu_sel.isnumeric() == False or not 1 <= int(menu_sel) <= 3:
+    while menu_sel.isnumeric() == False or not 1 <= int(menu_sel) <= 4:
         print("Invalid Command")
         menu_sel = input('> ')
         
     menu_sel = int(menu_sel)
                     
     if menu_sel == 1:
-        print('Attempting to open explorer')
+        path = filedialog.askopenfilename(
+            defaultextension = '~\AppData\Roaming\.minecraft\config\litematica',
+            filetypes = [("CSV Files", "*.csv")]
+        )
+        basename = os.path.basename(path) 
+        material_list_name, _ = os.path.splitext(basename) 
+        save_file_name = material_list_name + '.json'
+        save_file_path = 'saved-process/' + save_file_name
+        print(save_file_name)
+        print(save_file_path)
+        #check if new_process_selection.json already exists
         try:
-            path = easygui.fileopenbox(
-                #title='Select Material List',
-                #default='~\AppData\Roaming\.minecraft\config\litematica\*.csv',
-                #filetypes='*.csv'
-            )
-        except:
-            print('Failed to open explorer')
-        process_selection(resources, config, path)
-        #new_process_selection_json = open('new_process_selection_.json', 'x')
-    elif menu_sel == 2:
-        print("DO CONFIG FILE CONFIG WHEN YOU LEARN HOW TO EDIT YAML FILES")
-    elif menu_sel == 3:
-        print("DO FILE LOADING WHEN YOU GET AROUND TO WRITING FILES")
+            new_process_selection_json = open('saved-process/new_process_selection.json', 'x')
+            new_process_selection_json.close()
 
-startup_icon()
-menu(resources, config)
+            new_process_selection_json_setup = [
+                {
+                    "Material List": material_list_name,
+                    "Path": path
+                },
+            ]
+
+            with open('saved-process/new_process_selection.json', 'w') as new_process_selection_json:
+                json.dump(new_process_selection_json_setup, new_process_selection_json,
+                          indent=4,
+                          separators=(',',': '))
+            new_process_selection_json.close()
+        except:
+            print('Found existing startup file')
+            os.remove('saved-process/new_process_selection.json')
+            print('Existing startup file deleted')
+            new_process_selection_json = open('saved-process/new_process_selection.json', 'x')
+            new_process_selection_json.close()
+
+            new_process_selection_json_setup = [
+                {
+                    "Material List": save_file_name,
+                    "Path": path
+                },
+            ]
+
+            with open('saved-process/new_process_selection.json', 'w') as new_process_selection_json:
+                json.dump(new_process_selection_json_setup, new_process_selection_json,
+                          indent=4,
+                          separators=(',',': '))
+            new_process_selection_json.close()
+            print('Created new startup file')
+        try:
+            os.rename('saved-process/new_process_selection.json', save_file_path)
+        except:
+            print("It looks like there might already be a crafting list for this file")
+            print("Would you like to [L]oad the existing Crafting List, or create a [N]ew Crafting List?")
+            existing_list_input = input("> ")
+            existing_list_input = existing_list_input.lower().strip('oadew')
+            while existing_list_input not in ['l', 'n']:
+                print("Invalid Entry: Please select [L]oad or [N]ew file")
+                existing_list_input = input('> ')
+                existing_list_input = existing_list_input.lower().strip('oadew')
+            if existing_list_input == 'l':
+                load(save_file_path)
+            elif existing_list_input == 'n':
+                print(r'Would you like to delete the existing list? (Y\N)')
+                existing_list_input = input('> ')
+                existing_list_input = existing_list_input.lower().strip('eso')
+                while existing_list_input not in ['y', 'n']:
+                    print("Invalid Entry: Please select [Y]es or [N]o")
+                    existing_list_input = input('> ')
+                    existing_list_input = existing_list_input.lower().strip('eso')
+                if existing_list_input == 'y':
+                    os.remove(save_file_path)
+                    os.rename('saved-process/new_process_selection.json', save_file_path)
+                elif existing_list_input == 'n':
+                    try:
+                        existing_copy_number = '1'    #default first copy number
+                        save_file_path = 'saved-process/' + material_list_name + '-' + existing_copy_number + '.json'
+                        os.rename('saved-process/new_process_selection.json', save_file_path)
+                    except:
+                        existing_copy_number = ''.join(i for i in save_file_name if i.isdigit())
+                        existing_copy_number = str(int(existing_copy_number) + 1)
+                        save_file_path = 'saved-process/' + material_list_name + '-' + existing_copy_number + '.json'
+                        os.rename('saved-process/new_process_selection.json', save_file_path)
+
+        process_selection(resources, config, path, save_file_path)
+    elif menu_sel == 2:
+        print("DO CONFIG FILE")
+        print("DO ADD ITEM")
+    elif menu_sel == 3:
+        print("DO FILE LOADING")
+    elif menu_sel == 4:
+        os._exit(1)
+
+root.withdraw()
+root.attributes("-topmost", True)
+root.mainloop(
+    startup_icon(),
+    menu(resources, config)
+    )
